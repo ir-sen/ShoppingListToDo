fdd537d4315f354bde4bcb6a758c80888928577c
ir-sen <irsen.kan@bk.ru>
2021-08-05 11:26:44 +0300

    finish create ViewHolder in Recycle view

--- app/src/main/java/com/example/shoppinglisttodo/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopListAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.shoppinglisttodo.presentation
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglisttodo.R
 import com.example.shoppinglisttodo.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
@@ -11,18 +14,26 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.view.setOnLongClickListener {
+            true
+        }
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    class ShopItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
 
     }
 }
