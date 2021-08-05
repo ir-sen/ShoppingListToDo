a67e2e081a658f1eb0f9248ae735287520c56c41
ir-sen <irsen.kan@bk.ru>
2021-08-05 10:07:06 +0300

    create empty list in layout

--- app/src/main/java/com/example/shoppinglisttodo/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.shoppinglisttodo.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglisttodo.R
+import com.example.shoppinglisttodo.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewMode: MainViewModel
+    private lateinit var llShopList: LinearLayout
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,14 +22,22 @@ class MainActivity : AppCompatActivity() {
         // two equal method
         viewMode = ViewModelProvider(this)[MainViewModel::class.java]
 //        viewMode = ViewModelProvider(this).get(MainViewModel::class.java)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewMode.shopList.observe(this) {
-            Log.d("MainViewModelTest",  it.toString())
-            if (count == 0) {
-                val item = it[0]
-                viewMode.deleteShopItem(item)
-                count++
-            }
+            showList(it)
         }
 
     }
+    private fun showList(list: List<ShopItem>) {
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enable) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId, llShopList, false)
+            llShopList.addView(view)
+        }
+    }
+
 }
