e5b5f52fba463e0b44e3e4c33aae7b339c1f6eec
ir-sen <irsen.kan@bk.ru>
2021-08-05 11:11:53 +0300

    create adapter 1

---
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopListAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglisttodo.presentation
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    class ShopItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
