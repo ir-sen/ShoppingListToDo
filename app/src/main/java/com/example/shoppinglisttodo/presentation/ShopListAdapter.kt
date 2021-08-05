c28be45048faf81757f8b6f683ecaacb678d1c5e
ir-sen <irsen.kan@bk.ru>
2021-08-05 11:59:44 +0300

    create Recycle View 1

--- app/src/main/java/com/example/shoppinglisttodo/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopListAdapter.kt
@@ -12,12 +12,13 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
     val list = listOf<ShopItem>()
 
-
+    // из layout получаем view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
         return ShopItemViewHolder(view)
     }
 
+    // присваеваем к каждому элементу значения
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         val shopItem = list[position]
         viewHolder.tvName.text = shopItem.name
@@ -30,7 +31,7 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     override fun getItemCount(): Int {
         return list.size
     }
-
+    // Берем все view которые нам нужны
     class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
