5b3b8019edc99a6465ad068719c17cef775d3b5c
ir-sen <irsen.kan@bk.ru>
2021-08-06 23:07:55 +0300

    create  ShopItemViewModel.kt finish add get element use 4

--- app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.shoppinglisttodo.R
 
 class ShopItemActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
