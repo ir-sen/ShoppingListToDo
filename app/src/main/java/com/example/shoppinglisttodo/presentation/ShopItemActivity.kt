fdd564d8f7ab52c6a00df94f3c2bbc52bbd8523f
ir-sen <irsen.kan@bk.ru>
2021-08-06 11:36:52 +0300

    Create second layout Activity 1

---
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglisttodo.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppinglisttodo.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
