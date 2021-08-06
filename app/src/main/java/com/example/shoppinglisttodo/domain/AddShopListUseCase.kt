ed1219afb2afa4350dae56a4d9d4da2881290ccf
ir-sen <irsen.kan@bk.ru>
2021-08-06 13:51:47 +0300

    create  ShopItem view addShopItem 2

--- app/src/main/java/com/example/shoppinglisttodo/domain/AddShopListUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.shoppinglisttodo.domain
-
-class AddShopListUseCase(private val shopListRepository: ShopListRepository) {
-    fun addShopList(shopItem: ShopItem) {
-        shopListRepository.addShopList(shopItem)
-    }
-}
