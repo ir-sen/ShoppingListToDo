ec7497807aea5ddb56381a8f4fe709018e5b6f57
ir-sen <irsen.kan@bk.ru>
2021-08-10 13:47:30 +0300

    create close fragment 3

--- app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemFragment.kt
@@ -70,7 +70,7 @@ class ShopItemFragment(
         }
 
         viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
-            finish()
+            activity?.onBackPressed()
         }
     }
 
@@ -145,6 +145,14 @@ class ShopItemFragment(
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
         private const val MODE_UNKNOWN = ""
+
+        fun newInstanceAddItem(): ShopItemFragment {
+            return ShopItemFragment(ShopItemActivity.MODE_ADD)
+        }
+
+        fun newInstanceEditItem(shopItemId: Int): ShopItemFragment {
+            return ShopItemFragment(ShopItemActivity.MODE_EDIT, shopItemId)
+        }
 
         fun newIntentAddItem(context: Context) : Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
