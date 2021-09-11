3648f9635498266a1fa01db1fbb9fdedf8bd8680
ir-sen <irsen.kan@bk.ru>
2021-09-11 17:44:07 +0300

    init coroutine add dependencies in build.gradle

--- app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/example/shoppinglisttodo/presentation/ShopItemViewHolder.kt
@@ -2,11 +2,10 @@ package com.example.shoppinglisttodo.presentation
 
 import android.view.View
 import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglisttodo.R
+import com.example.shoppinglisttodo.databinding.ItemShopDisabledBinding
 
-class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
-    val tvName = view.findViewById<TextView>(R.id.tv_name)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-
-}
+class ShopItemViewHolder(val binding: ViewDataBinding)
+    : RecyclerView.ViewHolder(binding.root)
